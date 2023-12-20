b0e307fb376de3269efbc8372675edd58ab11bf5
Ryan Ziegler <rzig408@gmail.com>
2023-12-20 17:02:13 -0600

    add more parsing

--- app/Main.hs
+++ app/Main.hs
@@ -9,43 +9,82 @@ import Text.Parsec.Language
 import Text.Parsec.String (Parser)
 import Text.Parsec.Token
 
-data WeakPrimary = WeakTrue | WeakFalse | WeakNull | WeakNumber String | WeakString String | Identifier String deriving (Show, Eq, Ord)
-data WeakFunction = WeakPrimary
+data Expr
+  = WeakTrue
+  | WeakFalse
+  | WeakNull
+  | WeakNumber String
+  | WeakString String
+  | Identifier String
+  | Arguments [Expr]
+  deriving (Show, Eq, Ord)
 
-pIdentifier :: Parser WeakPrimary
+data Stmt
+  = Print Expr
+  | If Expr [Decl]
+  | Ret Expr
+  | ExprStmt Expr
+  deriving (Show, Eq, Ord)
+
+data Decl =
+  StmtDecl Stmt |
+  OpDecl Expr Expr Expr [Stmt] |
+  VarDecl Expr Expr |
+  FunDecl Expr Expr
+  deriving (Show, Eq, Ord)
+
+pIdentifier :: Parser Expr
 pIdentifier = do
   p1 <- letter <?> "Expected identifier to begin with letter"
-  p2 <- (alphaNum `endBy` spaces) <?> "Identifiers may only contain numbers and letters"
+  p2 <- many1 alphaNum
+  -- p2 <- (alphaNum `endBy` (spaces <|> try (string ";"))) <?> "Identifiers may only contain numbers and letters"
   return (Identifier (p1 : p2))
 
-pNumber :: Parser WeakPrimary
-pNumber = do
-  n <- many1 digit
-  return (WeakNumber n)
+pNumber :: Parser Expr
+pNumber = do WeakNumber <$> many1 digit
 
-pString :: Parser WeakPrimary
-pString = do
-  p <- stringLiteral (makeTokenParser haskellDef)
-  return (WeakString p)
+pString :: Parser Expr
+pString = do WeakString <$> stringLiteral (makeTokenParser haskellDef)
 
-pTrue :: Parser WeakPrimary
+pTrue :: Parser Expr
 pTrue = do
   _ <- char 'T' <* notFollowedBy alphaNum
   return WeakTrue
 
-pFalse :: Parser WeakPrimary
+pFalse :: Parser Expr
 pFalse = do
   _ <- char 'F' <* notFollowedBy alphaNum
   return WeakFalse
 
-pNull :: Parser WeakPrimary
+pNull :: Parser Expr
 pNull = do
   _ <- char 'N'
   return WeakNull
 
-pPrimary :: Parser WeakPrimary
-pPrimary = do
+pExpr :: Parser Expr
+pExpr = do
   try pTrue <|> try pFalse <|> try pNull <|> try pString <|> try pNumber <|> try pIdentifier
+
+pPrint :: Parser Stmt
+pPrint = do char 'p' *> spaces *> (Print <$> pExpr) <* char ';'
+
+pIf :: Parser Stmt
+pIf = do
+  cond <- char 'i' *> spaces *> between (char '(') (char ')') pExpr
+  If cond <$> pBlock
+
+pExprStmt :: Parser Stmt
+pExprStmt = do
+  ExprStmt <$> pExpr <* char ';'
+
+pStmt :: Parser Stmt
+pStmt = do try pIf <|> try pExprStmt <|> try pPrint
+
+pDecl :: Parser Decl
+pDecl = do StmtDecl <$> try pStmt
+
+pBlock :: Parser [Decl]
+pBlock = do spaces *> char '{' *> spaces *> pDecl `sepBy` spaces <* char '}'
 
 stringify :: (Show b) => Either ParseError b -> String
 stringify x =
@@ -54,4 +93,4 @@ stringify x =
     Right b -> show b
 
 main :: IO ()
-main = putStrLn (stringify (parse pPrimary "file.txt" "T"))
+main = putStrLn (stringify (parse pDecl "file.txt" "i(T) {p 123;}"))
